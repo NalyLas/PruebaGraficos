8bf4a530f6f5be7158169991818e6ce81ee78143
ptmarketing04 <uweb@megasur.es>
2017-04-19 17:18:12 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ptmarketing04/pruebagraficos/MainActivity.java
@@ -0,0 +1,134 @@
+package com.example.ptmarketing04.pruebagraficos;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private PieChart pieChart;
+    private BarChart barChart;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        pieChart = (PieChart) findViewById(R.id.pieChart);
+        barChart = (BarChart) findViewById(R.id.barChart);
+
+        /*definimos algunos atributos*/
+        pieChart.setHoleRadius(40f);
+        pieChart.setDrawYValues(true);
+        pieChart.setDrawXValues(true);
+        pieChart.setRotationEnabled(true);
+        pieChart.animateXY(1500, 1500);
+
+		/*creamos una lista para los valores Y*/
+        ArrayList<Entry> valsY = new ArrayList<Entry>();
+        valsY.add(new Entry(5* 100 / 25,0));
+        valsY.add(new Entry(20 * 100 / 25,1));
+
+ 		/*creamos una lista para los valores X*/
+        ArrayList<String> valsX = new ArrayList<String>();
+        valsX.add("Varones");
+        valsX.add("Mujeres");
+
+ 		/*creamos una lista de colores*/
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+        colors.add(getResources().getColor(R.color.colorAccent));
+        colors.add(getResources().getColor(R.color.colorPrimary));
+
+ 		/*seteamos los valores de Y y los colores*/
+        PieDataSet set1 = new PieDataSet(valsY, "Resultados");
+        set1.setSliceSpace(3f);
+        set1.setColors(colors);
+
+		/*seteamos los valores de X*/
+        PieData data = new PieData(valsX, set1);
+        pieChart.setData(data);
+        pieChart.highlightValues(null);
+        pieChart.invalidate();
+
+        /*Ocutar descripcion*/
+        pieChart.setDescription("");
+        /*Ocultar leyenda*/
+        pieChart.setDrawLegend(false);
+
+
+
+
+        BarData data2 = new BarData(getXAxisValues(), getDataSet());
+        barChart.setData(data2);
+        barChart.setDescription("miau miau miaus");
+        barChart.animateXY(2000, 2000);
+        barChart.invalidate();
+
+
+
+    }
+
+    private ArrayList<BarDataSet> getDataSet() {
+        ArrayList<BarDataSet> dataSets = null;
+
+        ArrayList<BarEntry> valueSet1 = new ArrayList<>();
+        BarEntry v1e1 = new BarEntry(110.000f, 0); // Jan
+        valueSet1.add(v1e1);
+        BarEntry v1e2 = new BarEntry(40.000f, 1); // Feb
+        valueSet1.add(v1e2);
+        BarEntry v1e3 = new BarEntry(60.000f, 2); // Mar
+        valueSet1.add(v1e3);
+        BarEntry v1e4 = new BarEntry(30.000f, 3); // Apr
+        valueSet1.add(v1e4);
+        BarEntry v1e5 = new BarEntry(90.000f, 4); // May
+        valueSet1.add(v1e5);
+        BarEntry v1e6 = new BarEntry(100.000f, 5); // Jun
+        valueSet1.add(v1e6);
+
+        ArrayList<BarEntry> valueSet2 = new ArrayList<>();
+        BarEntry v2e1 = new BarEntry(150.000f, 0); // Jan
+        valueSet2.add(v2e1);
+        BarEntry v2e2 = new BarEntry(90.000f, 1); // Feb
+        valueSet2.add(v2e2);
+        BarEntry v2e3 = new BarEntry(120.000f, 2); // Mar
+        valueSet2.add(v2e3);
+        BarEntry v2e4 = new BarEntry(60.000f, 3); // Apr
+        valueSet2.add(v2e4);
+        BarEntry v2e5 = new BarEntry(20.000f, 4); // May
+        valueSet2.add(v2e5);
+        BarEntry v2e6 = new BarEntry(80.000f, 5); // Jun
+        valueSet2.add(v2e6);
+
+        BarDataSet barDataSet1 = new BarDataSet(valueSet1, "Brand 1");
+        barDataSet1.setColor(getResources().getColor(R.color.colorPrimary));
+        BarDataSet barDataSet2 = new BarDataSet(valueSet2, "Brand 2");
+        barDataSet2.setColor(getResources().getColor(R.color.colorAccent));
+
+        dataSets = new ArrayList<>();
+        dataSets.add(barDataSet1);
+        dataSets.add(barDataSet2);
+        return dataSets;
+    }
+
+    private ArrayList<String> getXAxisValues() {
+        ArrayList<String> xAxis = new ArrayList<>();
+        xAxis.add("JAN");
+        xAxis.add("FEB");
+        xAxis.add("MAR");
+        xAxis.add("APR");
+        xAxis.add("MAY");
+        xAxis.add("JUN");
+        return xAxis;
+    }
+}
